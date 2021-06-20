0e4718b0daf809975d0179b1151ed7d480a77475
SumankrishnaNakka <78990204+SumankrishnaNakka@users.noreply.github.com>
2021-06-20 19:37:08 -0400

    changes made

--- src/main/java/com/companyname/bank/inbox.java
+++ src/main/java/com/companyname/bank/inbox.java
@@ -8,6 +8,6 @@ public class inbox
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Fucking World!" );
     }
 }
